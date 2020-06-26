321a315f07d1c5319bf980650207701adc29b554
asish <iamasish3001@gmail.com>
2020-06-26 19:47:38 +0530

    added more codes

---
+++ class_object.py
@@ -0,0 +1,17 @@
+''' 
+class = blueprint  and object = properties
+
+
+'''
+
+
+class Computer:
+    def config(self):
+        print("something something")
+
+
+com1 = Computer()
+
+#com1 is the object for the class Computer
+
+print(type(com1))
