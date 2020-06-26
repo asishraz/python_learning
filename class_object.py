30ee1035e1bb3e1fa57da398cf49493d64d09c65
asish <iamasish3001@gmail.com>
2020-06-26 20:09:59 +0530

    added more codes

--- class_object.py
+++ class_object.py
@@ -1,17 +1,43 @@
 ''' 
 class = blueprint  and object = properties
+class is the combination of methods(functions) and variables 
+functions in OOPS called methods
 
+a class class have many objects
 
-'''
+everything in python is object
+in order to create your own class, we use the keyword 'class'
+
 
 
 class Computer:
     def config(self):
         print("something something")
 
+com1 = Computer() #com1 is the object for the class Computer
+
+print(type(com1))
+x = 9
+y = 'str'
+print(type(x))
+print(type(y))
+
+
+now we will call the object of the class and in order to call the config method inside the class, we need to mention the object(com1) by the time of calling
+'''
+
+class Computer:
+    def config(self):
+        print("something something")
 
 com1 = Computer()
 
-#com1 is the object for the class Computer
+Computer.config(com1) #this config will go inside the class as function which has the argument 'com1' and will print the next line  
+#and another way of calling the method 'config':
 
-print(type(com1))
+com1.config()
+
+
+
+
+
