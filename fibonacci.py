c81127f115bacd66c9d0f2ed3bcba86228ea3242
asish <iamasish3001@gmail.com>
2020-07-01 07:56:22 +0530

    learning how to write fibonacci series

---
+++ fibonacci.py
@@ -0,0 +1,20 @@
+'''
+fibonacci sequence
+0 1 1 2 3 5 8 13 21 ...
+
+
+0+1 = 1 #the next number
+1+1 = 2 #the next number
+2+1 = 3
+and so on ...
+
+'''
+
+
+def fibonacci(num):
+    a = 0
+    b = 1
+    print(a+b)
+
+
+fibonacci(5)
