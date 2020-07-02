d9449b2179884289de21a2deb288e1a170800404
asish <iamasish3001@gmail.com>
2020-07-02 16:13:50 +0530

    learn how to use lambda in filter function

--- filter_function.py
+++ filter_function.py
@@ -1,14 +1,47 @@
-''' use of filter function, lambda and the map
+''' use of filter function
+
+
+def is_even(num):
+    return num%2 == 0
+numbers = [1,12,3,53,5,64,64,62,62,646,42,63246,464,634,6464,36,3464]
+
+evens = list(filter(is_even,numbers))
+print(evens)
+
+vowels = ['a','e','i','o','u']
+
+strings = ['a','a','s','k','j','d','f','o','a','i','w','e','u','f','k','l','p','e','i','f','j','s','d','k','l','f','j','d','s','k','l','f']
+def is_vowel(alpha):
+    return alpha in vowels
+    
+
+result = list(filter(is_vowel,strings))
+
+print(result)
 
 '''
 
 
-numbers = [1,2,4,1,3,2,4,2,5,6,4,6,7,8,8,9]
+''' use of lambda function 
 
-def is_even(num):
-    return num % 2 == 0
+numbers = [1,12,3,53,5,64,64,62,62,646,42,63246,464,634,6464,36,3464]
+vowels = ['a','e','i','o','u']
+strings = ['a','a','s','k','j','d','f','o','a','i','w','e','u','f','k','l','p','e','i','f','j','s','d','k','l','f','j','d','s','k','l','f']
 
-even = list(filter(is_even,numbers))
 
-print(even)
+
+even_list = list(filter(lambda x:x%2==0, numbers))
+print(even_list)
+
+vowel_list = list(filter(lambda a:a in vowels, strings))
+print(vowel_list)
+
+
+
+'''
+
+''' use of map function
+'''
+
+
 
