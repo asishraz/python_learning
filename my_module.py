bc068eb2989289a45720464a93a41c70607f08b7
asish <iamasish3001@gmail.com>
2020-07-03 08:19:05 +0530

    learning about modules in python

--- my_module.py
+++ my_module.py
@@ -10,4 +10,22 @@ def div(a,b):
 def mult(a,b):
     return a*b
 
-    
+
+def smart_div(func):
+    def inner(a,b):
+        if a<b:
+            a,b = b,a
+        return func(a,b)
+    return inner
+
+div = smart_div(div)
+
+
+def smart_sub(func):
+    def inner(a,b):
+        if a < b:
+            print("We are not printing the negative result, therefore swapping the values of both the variables")
+            a,b = b,a
+        return func(a,b)
+    return inner
+sub = smart_sub(sub)
